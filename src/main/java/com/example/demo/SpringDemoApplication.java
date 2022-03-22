c311838a6916dec64c240f9ae4c8720f563d56fe
Nabila63Ahmed <nabila.ahmed63@gmail.com>
2022-03-22 17:13:30 +0100

    :construction: POST:postGreetings

--- src/main/java/com/example/demo/SpringDemoApplication.java
+++ src/main/java/com/example/demo/SpringDemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedList;
 import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
@@ -41,10 +42,17 @@ public class SpringDemoApplication {
 		return counter.addAndGet(increment);
 	}
 
-	/*Post Request*/
+	/*Post Request: Body automatically type-casted*/
 	@PostMapping("/postGreeting")
-	public Greeting postGreeting(@RequestBody Greeting greeting){
+	public Greeting postGreeting(@RequestBody Greeting greeting) {
 		System.out.print(greeting.getContent());
 		return greeting;
+	}
+
+	/*Post Request: List in body, choose an item and greet with*/
+	@PostMapping("/postGreetings")
+	public String postGreetings(@RequestParam(value = "index", defaultValue = "0") int index, @RequestBody Greeting[] greetings) {
+		String name = greetings!=null & index < greetings.length? greetings[index].getContent(): "World";
+		return String.format("Hello %s!", name);
 	}
 }
